3e190c157343d822e3a2563cedd409f174a33efb
Aelson Carvalho <aelsoncg22@hotmail.com>
2022-07-15 08:35:55 -0300

    trocando java por C# :)

---
+++ C#/Calculadora/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("#######################");
+        Console.WriteLine("Bem vindo a calculadora");
+        Console.WriteLine("#######################");
+        bool ligado = true;
+
+
+        Console.WriteLine("Digite um numero");
+        float n1 = float.Parse(Console.ReadLine());
+        float res;
+        float arm = n1;
+
+        while (ligado)
+        {
+            if (ligado)
+            {
+                Console.WriteLine("Escolha a operação: \n[+] Soma\n[-] Subtração\n[*] Multiplicação\n[/] Divisão");
+                string escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "+":
+                        float n2 = float.Parse(Console.ReadLine());
+                        res = arm + n2;
+                        Console.WriteLine("A soma entre {0} e {1} é {2}", arm, n2, res);
+                        break;
+                    case "-":
+
+                        break;
+                    default:
+                        Console.WriteLine("Operação invalida");
+                        break;
+                }
+            }
+        }
+    }
+}
--- Java/OlaMundo.java
+++
@@ -1,7 +0,0 @@
-package Java;
-
-public class OlaMundo {
-    public static void main(String[] args){
-        System.out.print("Ola mundo");
-    }
-}
