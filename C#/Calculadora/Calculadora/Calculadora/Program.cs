22b154db8442dbf09009c6178210086327aa773d
Aelson Carvalho <aelsoncg22@hotmail.com>
2022-07-15 08:38:14 -0300

    Corração da calculadora

--- C#/Calculadora/Calculadora/Calculadora/Program.cs
+++ C#/Calculadora/Calculadora/Calculadora/Program.cs
@@ -6,12 +6,13 @@ class Program
     {
         Console.WriteLine("#######################");
         Console.WriteLine("Bem vindo a calculadora");
-        Console.WriteLine("#######################");
+        Console.WriteLine("#######################\n\n");
         bool ligado = true;
 
 
         Console.WriteLine("Digite um numero");
         float n1 = float.Parse(Console.ReadLine());
+        float n2;
         float res;
         float arm = n1;
 
@@ -25,12 +26,32 @@ class Program
                 switch (escolha)
                 {
                     case "+":
-                        float n2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = float.Parse(Console.ReadLine());
                         res = arm + n2;
                         Console.WriteLine("A soma entre {0} e {1} é {2}", arm, n2, res);
+                        arm = res;
                         break;
                     case "-":
-
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = float.Parse(Console.ReadLine());
+                        res = arm - n2;
+                        Console.WriteLine("A subtração entre {0} e {1} é {2}", arm, n2, res);
+                        arm = res;
+                        break;
+                    case "*":
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = float.Parse(Console.ReadLine());
+                        res = arm * n2;
+                        Console.WriteLine("A subtração entre {0} e {1} é {2}", arm, n2, res);
+                        arm = res;
+                        break;
+                    case "/":
+                        Console.WriteLine("Digite outro numero:");
+                        n2 = float.Parse(Console.ReadLine());
+                        res = arm / n2;
+                        Console.WriteLine("A subtração entre {0} e {1} é {2}", arm, n2, res);
+                        arm = res;
                         break;
                     default:
                         Console.WriteLine("Operação invalida");
