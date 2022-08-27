010653e3f30a8496530c17c666fdc72210cb2470
Aelson Carvalho <aelsoncg22@hotmail.com>
2022-08-27 15:46:32 -0300

    estudo de asp.net

---
+++ ControleDeContatos/ControleDeContatos/Controllers/Contato.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class Contato : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+    }
+}
